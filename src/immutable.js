2d4f8865e2b6b4e185c2ca2906b2ac146d95f706
Code Swimmer <codeswimmer@gmail.com>
2013-08-03 09:24:31 -0700

    Add createUsingEveryOtherFrom() and makeEveryOther() to immutableArray

--- src/immutable.js
+++ src/immutable.js
@@ -84,7 +84,7 @@ immutableArray = function(array) {
     that.forEach(function(element, index) {
       result += element;
       if (index < lockedArray.length - 1)
-        result += ', ';
+        result += ',';
     });
     return result;
   }
@@ -93,10 +93,45 @@ immutableArray = function(array) {
   return that;
 };
 
-immutableArray.createFilledWith = function(thing, times) {
+immutableArray.createFilledWith = function(times, thing) {
   var array = [times];
   for (var i = 0; i < times; i++) {
     array[i] = thing;
   };
   return immutableArray(array);
 };
+
+/*
+Returns an immutableArray containing every other item from the source.
+*/
+immutableArray.createUsingEveryOtherFrom = function(source) {
+  var everyOther = immutableArray.makeEveryOther(2, source);
+  return everyOther();
+};
+
+/*
+Returns a function that returns an immutableArray containing every other N item
+from the source.
+*/
+immutableArray.makeEveryOther = function(n, source) {
+  return function(){
+    return everyOtherN(n, source)
+  };
+};
+
+/*
+Returns an immutableArray that contains every other N items from sourceArray
+*/
+var everyOtherN = function(n, sourceArray) {
+  var array = [];
+
+  sourceArray.forEach(function(element, index) {
+    if (index % n == 0) {
+      array.push(element);
+    };
+  })
+
+  return immutableArray(array);
+};
+
+
--- test/immutableSpec.js
+++ test/immutableSpec.js
@@ -284,17 +284,37 @@ describe('immutable', function(){
       it('should create a new immutableArray that has the requested number of items', function(){
         var thing = '.';
         var times = 10;
-        var lockedArray = immutableArray.createFilledWith(thing, times);
+        var lockedArray = immutableArray.createFilledWith(times, thing);
         assert(lockedArray != null);
         assert(lockedArray.length == times);
       });
 
       it('should create a new immutableArray that is only filled with the given item', function(){
-        var thing = '.';
+        var thing = '-';
         var times = 10;
         var lockedArray = immutableArray.createFilledWith(thing, times);
         var result = lockedArray.every(function(element) { return element === thing; });
         assert(result);
+      });
+    });
+
+    describe('#createUsingEveryOtherFrom', function(){
+      it('should create a new immutableArray that is filled with every other item from the source array', function(){
+        var lockedArray = createAggregatedLockedArray();
+        var result = immutableArray.createUsingEveryOtherFrom(lockedArray);
+        var expectedNumItems = Math.round(lockedArray.length / 2);
+        assert(result.length == expectedNumItems);
+      });
+    });
+
+    describe('#makeEveryOther', function(){
+      it('should create a function that allows me to skip every third item', function(){
+        var lockedArray = createAggregatedLockedArray();
+        var n = 3;
+        var everyThird = immutableArray.makeEveryOther(n, lockedArray);
+        var result = everyThird();
+        var expectedNumItems = Math.round(lockedArray.length / 3);
+        assert(result.length == expectedNumItems);
       });
     });
   });
@@ -320,4 +340,9 @@ describe('immutable', function(){
     var result = lockedArray.contains(target);
     assert(result == false);
   };
+
+  function createAggregatedLockedArray() {
+    var sourceArray = array.concat(numbers).concat(letters);
+    return immutableArray(sourceArray);
+  }
 });
